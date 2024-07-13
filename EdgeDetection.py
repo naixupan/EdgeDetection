6ba725efe6344490e42a92f5a76c40b2ad7bd05e
heguangpeng <86814805+naixupan@users.noreply.github.com>
2024-07-13 14:27:55 +0800

    边缘检测更新

--- EdgeDetection.py
+++ EdgeDetection.py
@@ -2,75 +2,70 @@
 # 文件名称：EdgeDetection.py
 # 功能描述：对花篮图片进行边缘检测，获取各区域的面积，并统计数量。其他需求后续处理
 # 开发人员：何广鹏
+# 更新日期：2024年7月13日
+# 更新内容：修改了斑点检测的检测策略和显示的方法
 
 import cv2
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 
+from DisplayImages import display_images
 
-# 读取图片；将图像转化为灰度图
+# 读取待检测图像和模板图片
+image_path = 'F:/images/result/result_pic/cuopian/20240524_071125709_0.BMP'
+template_path = './ModelImages/Crop_Model_Mid.bmp'
 
-image_dir='F:/images/result/result_pic/cuopian/20240524_071125709_0.BMP'
-image = cv2.imread(image_dir,cv2.IMREAD_GRAYSCALE)   # 使用OpenCV读取图像，并以灰度模式加载图像
-cv2.imshow("image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+image = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)     #读取图片并转化为灰度图
+template = cv2.imread(template_path,cv2.IMREAD_GRAYSCALE)
 
-print(image)
+########################        模板匹配        ########################
+# 进行模板匹配
+result = cv2.matchTemplate(image,template,cv2.TM_CCOEFF_NORMED)
 
-# 裁剪
+# 获取匹配结果中得分最高的位置
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-cropped_image=image[561:869, 939:10719]
+# 获取模板的宽度和高度
+template_width = template.shape[1]
+template_height = template.shape[0]
 
-# cv2.imshow("corpped_image", cropped_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# 确定裁剪区域
+top_left = max_loc
+bottom_right = (top_left[0] + template_width,top_left[1] + template_height)
+
+# 裁剪图像
+cropped_image = image[top_left[1]:bottom_right[1],top_left[0]:bottom_right[0]]
 
 # 图像二值化
-
 ret,binatied_image=cv2.threshold(cropped_image, 80, 180, cv2.THRESH_BINARY);
 
-# cv2.imshow("binatied_image", binatied_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# 腐蚀操作
+erode_image = cv2.erode(binatied_image,(5,5))
+plt.imshow(erode_image, cmap='gray')
+plt.show()
 
 # 平滑滤波
-blur_image = cv2.GaussianBlur(binatied_image,(3,3),0)
+blur_image = cv2.GaussianBlur(erode_image,(3,3),0)
 
-cv2.imshow("blur_image", blur_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+edges = cv2.Canny(blur_image, 50, 150)
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+num_blobs = len(contours)
 
-
-# 边缘检测/斑点检测
-
-edge_image = cv2.Canny(blur_image,100,200)
-
-cv2.imshow("edge_image", edge_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# 查找轮廓
-contours, _ = cv2.findContours(edge_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# 统计轮廓数量即图形数量
-count = len(contours)
-
-print(count)
+drewed_image=np.copy(cropped_image)
 
 for contour in contours:
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(drewed_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
     area = cv2.contourArea(contour)
-    print("area:",area)
+    print(f"当前斑点的面积: {area}")
 
 
-# 读取二值图像
-# binary_image = cv2.imread('binary_image.jpg', cv2.IMREAD_GRAYSCALE)
+#显示效果
+# plt.imshow(cropped_image, cmap='gray')
+# plt.show()
+print(f"Number of detected blobs: {num_blobs}")
 
-# 标记连通区域
-num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(binatied_image)
-
-# 统计图形数量
-count = num_labels - 1  # 减去背景
-
-print(count)
+images=[image,cropped_image,binatied_image,erode_image,blur_image,edges,drewed_image]
+titles=["Original_image","cropped_image","binatied_image","erode_image","blur_image","edges","drewed_image"]
+display_images(images,titles)
